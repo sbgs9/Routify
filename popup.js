e51fe19a15064b679ad5fad9c99fce200250cad1
Ganesh Susarla <ganeshsusarla01@gmail.com>
2021-07-28 01:29:06 -0700

    Added sweetalert2 and example popup, have to fix UI dimensions

--- popup.js
+++ popup.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
     $("#btn-optimize").click(function() {
-        var alertMessage = "Your route is now optimized. Thanks for using Routify!";
-        alert(alertMessage);
+        //var alertMessage = "Your route is now optimized. Thanks for using Routify!";
+        //alert(alertMessage);
+        
+        Swal.fire(
+            'Thanks for using Routify!',
+            'Your route is now optimized!',
+            'success'
+        )
+        
     });
 });
