4d8d6afbad092420473fc983d3ce5b171fae8996
Ganesh Susarla <ganeshsusarla01@gmail.com>
2021-07-30 01:05:51 -0700

    updated Graph.js to include weights in order to support djikstra's algorithm

--- js/Graph.js
+++ js/Graph.js
@@ -1,8 +1,11 @@
 class Graph {
     // constructor for undirected graph
-    constructor(numValues) {
-        this.numValues = numValues;
+    constructor() {
         this.adjList = new Map();
+    }
+
+    get adjList() {
+        return this.adjList;
     }
 
     //Adds a vertex with the specified value in the adjacency list
@@ -11,16 +14,16 @@ class Graph {
     }
 
     //Takes two vertices and adds an edge between them so they are able to go in both directions
-    addEdge(vertex, otherVertex) {
-        this.adjList.get(vertex).push(otherVertex);
-        this.adjList.get(otherVertex).push(vertex);
+    addEdge(vertex, otherVertex, weight) {
+        this.adjList.get(vertex).push({otherVertex: otherVertex, weight: weight});
+        this.adjList.get(otherVertex).push({otherVertex: vertex, weight: weight});
     }
     
     print() {
         for(let key of this.adjList.keys()) {
             let values = "";
             for(let value of this.adjList.get(key)) {
-                values += value + " ";
+                values += value.otherVertex + "," + value.weight + " ";
             }
             console.log(key + " => " + values);
         }
