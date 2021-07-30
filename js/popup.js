cb8649215ceea8c41fe45bb109514c36fbaa5a40
Ganesh Susarla <ganeshsusarla01@gmail.com>
2021-07-30 16:38:04 -0700

    Added dark mode to switch to UI and added dark mode functionality in popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,16 @@
  $(document).ready(function() {
+     let checkBox = document.querySelector('input[type="checkbox"]');
+     checkBox.addEventListener('change', function() {
+         if(checkBox.checked) {
+            $('body').css('background-color', '#6404ec');
+            $('#text-routify').css('color', 'aquamarine');
+            $('#btn-optimize').css('background-color', 'aquamarine');
+         } else {
+            $('#btn-optimize').css('background-color', '#6404ec');
+            $('#text-routify').css('color', '#6404ec');
+            $('body').css('background-color', 'aquamarine');
+         }
+     })
     $("#btn-optimize").click(function() {
         $('body').css('height', '450');
         $('body').css('background-color', '#6404ec');
