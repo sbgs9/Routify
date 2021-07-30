b4cc179fa6f9a60692e2925f96d54c17215d4209
Ganesh Susarla <ganeshsusarla01@gmail.com>
2021-07-29 23:28:01 -0700

    Added changing color of UI when popup appears and started working on backend to get data from google maps

---
+++ js/content.js
@@ -0,0 +1,4 @@
+let searchboxes =  document.getElementsByClassName('tactile-searchbox-input');
+for(elt of searchboxes) {
+    console.log(elt.ariaLabel);
+}
--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,18 @@
-$(document).ready(function() {
+ $(document).ready(function() {
     $("#btn-optimize").click(function() {
         $('body').css('height', '450');
+        $('body').css('background-color', '#6404ec');
+        $('#text-routify').css('color', 'aquamarine');
+        $('#btn-optimize').css('background-color', 'aquamarine');
         cuteAlert({
             type: "success",
             title: "Thanks for using Routify!",
             message: "Your route is now optimized!",
             buttonText: "Woohoo!"
         }).then(function() {
+            $('#btn-optimize').css('background-color', '#6404ec');
+            $('#text-routify').css('color', '#6404ec');
+            $('body').css('background-color', 'aquamarine');
             $('body').css('height', '200');
         });
     });
